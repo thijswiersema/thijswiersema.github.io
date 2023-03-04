766f7a93b88ff4a1e1d4b0c7c3694b60246bbf7f
Thijs Wiersema <thijswiersema@gmail.com>
2023-03-04 11:10:25 +0100

    Initial site setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'tlgrey': '#eeeadf',
+        'tbrown': '#d3c9ad',
+        'torange': '#F39919',
+        'tblack': '#342825',
+        'tdbrown': '#796A64',
+      }
+    },
+
+  },
+  plugins: [],
+}
